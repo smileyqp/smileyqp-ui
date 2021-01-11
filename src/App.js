da5e86183cbecc27ee88b8febb9ecacb32d7ffba
smileyqp <smileyqp@gmail.com>
2021-01-11 15:25:37 +0800

    polish

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import { Button } from './components';
+
+function App() {
+  return (
+    <div className="App">
+      <Button text={'hhhasdasdh'}></Button>
+    </div>
+  );
+}
+
+export default App;
